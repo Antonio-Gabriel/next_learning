089b559bbfcd64b453b13453d8f24faeeaad2905
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2021-12-21 19:11:15 +0100

    configure global styles and head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,15 @@
-import styles from "../styles/home.module.scss";
+import Head from "next/head";
 
 export default function Home() {
-  return <h1 className={styles.title}>Welcome to next</h1>;
+  return (
+    <>
+      <Head>
+        <title>Inicion | ig.news </title>
+      </Head>
+      <h1>
+        Welcome to next
+        <span>You'r welcome</span>
+      </h1>
+    </>
+  );
 }
